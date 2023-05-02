2c223f98ebb6a5b2246e22ef21cdba5db8eda4dd
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-05-02 14:28:24 +0600

    check selected data and send it

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const chefData = require("./data/chef.json");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("hello world");
@@ -10,6 +13,12 @@ app.get("/", (req, res) => {
 
 app.get("/chef", (req, res) => {
   res.send(chefData);
+});
+
+app.get("/chef/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const selectedChef = chefData.find((sc) => sc.id === id); // sc = single chef
+  res.send(selectedChef);
 });
 
 app.listen(port, () => {
