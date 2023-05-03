0c55d1beacd7e84fde69341117cd3a7d85ee2f3c
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-05-03 14:05:54 +0600

    added new json data

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 const chefData = require("./data/chef.json");
+const country = require("./data/country.json");
 
 app.use(cors());
 
@@ -19,6 +20,10 @@ app.get("/chef/:id", (req, res) => {
   const id = parseInt(req.params.id);
   const selectedChef = chefData.find((sc) => sc.id === id); // sc = single chef
   res.send(selectedChef);
+});
+
+app.get("/country", (req, res) => {
+  res.send(country);
 });
 
 app.listen(port, () => {
